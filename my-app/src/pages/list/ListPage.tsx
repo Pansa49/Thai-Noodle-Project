dfb7c16dfc1db42f1b5040c40921dd7c790a6c46
Ping Pansa <Pansa.SaeUe@gmail.com>
2026-02-06 17:46:38 +0700

    update listpage

--- my-app/src/pages/list/ListPage.tsx
+++ my-app/src/pages/list/ListPage.tsx
@@ -1,8 +1,21 @@
-export function ListPage() {
+import db from "../../database/db.json";
 
+export function ListPage() {
     return (
-        <div className="container py-12 space-y-8">
-            ListPage
+        <div className="container py-12 space-y-4">
+            {db.orders.map((order) => (
+                <div
+                    key={order.id}
+                    className="border rounded-lg p-4"
+                >
+                    <p className="font-bold">{order.soup}
+                        {order.noodle}
+                        {order.meat.map((m, i) => (
+                            <span key={i}>{m}</span>
+                        ))}</p>
+                    <p className="text-gray-600">{order.totalPrice} บาท</p>
+                </div>
+            ))}
         </div>
-    )
+    );
 }
