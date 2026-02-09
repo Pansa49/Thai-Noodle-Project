9ecfb69b27b0b7f97f365098a23f2913851e5d8e
Ping Pansa <Pansa.SaeUe@gmail.com>
2026-02-09 15:02:52 +0700

    update listorder, listpage

--- my-app/src/components/ListOrder.tsx
+++ my-app/src/components/ListOrder.tsx
@@ -3,6 +3,8 @@ import { useCartContext } from "../hook/use-cart-context";
 export function ListOrder() {
     const { items } = useCartContext();
 
+
+
     if (items.length === 0) {
         return (
             <p className="text-gray-500 text-center py-12">
@@ -13,22 +15,18 @@ export function ListOrder() {
 
     return (
         <div className="container py-12 space-y-4">
-            {items.map((item) => (
+            {items.map((item, inddex) => (
                 <div
-                    key={item.id}
+                    key={inddex}
                     className="border rounded-lg p-4"
                 >
                     <p className="font-bold">{item.soup}
                         {item.noodle}
                         {item.meat.map((m, i) => (
-                            <span key={i}>{m}</span>
-                        ))}</p>
+                            <span key={i}>{m} </span>
+                        ))}
+                    </p>
                     <p className="text-gray-600">{item.totalPrice} บาท</p>
-                    <button
-                        className="w-8 h-8 rounded-full bg-blue-500 text-white text-lg"
-                    >
-                        🗑
-                    </button>
                 </div>
             ))
             }
--- my-app/src/pages/list/ListPage.tsx
+++ my-app/src/pages/list/ListPage.tsx
@@ -1,21 +1,18 @@
-import db from "../../database/db.json";
+import { ListOrder } from "../../components/ListOrder";
+import { useCartContext } from "../../hook/use-cart-context";
 
 export function ListPage() {
+
     return (
         <div className="container py-12 space-y-4">
-            {db.orders.map((order) => (
-                <div
-                    key={order.id}
-                    className="border rounded-lg p-4"
-                >
-                    <p className="font-bold">{order.soup}
-                        {order.noodle}
-                        {order.meat.map((m, i) => (
-                            <span key={i}>{m}</span>
-                        ))}</p>
-                    <p className="text-gray-600">{order.totalPrice} บาท</p>
-                </div>
-            ))}
+            List Page
+            <ListOrder />
+
+            <button className="mt-4 w-full py-2 rounded-xl bg-blue-500 text-white hover:bg-blue-600 active:scale-95 transition">
+                ส่งรายการอาหาร
+            </button>
         </div>
     );
 }
+
+
