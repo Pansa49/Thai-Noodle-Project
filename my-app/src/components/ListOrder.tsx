b3017aab820f4e54aa50906c187c185bf1147fa4
Ping Pansa <Pansa.SaeUe@gmail.com>
2026-02-12 22:30:21 +0700

    update ListOrder

--- my-app/src/components/ListOrder.tsx
+++ my-app/src/components/ListOrder.tsx
@@ -13,18 +13,28 @@ export function ListOrder() {
 
     return (
         <div className="container py-12 space-y-4">
-            {items.map((item, inddex) => (
+            {items.map((item, index) => (
                 <div
-                    key={inddex}
-                    className="border rounded-lg p-4"
+                    key={index}
+                    className="group border rounded-lg p-4 flex justify-between items-center transition hover:shadow-md"
                 >
-                    <p className="font-bold">{item.soup}
-                        {item.noodle}
-                        {item.meat.map((m, i) => (
-                            <span key={i}>{m} </span>
-                        ))}
-                    </p>
-                    <p className="text-gray-600">{item.totalPrice} บาท</p>
+                    <div>
+                        <p className="font-bold">
+                            {item.soup}
+                            {item.noodle}
+                            {item.meat.map((m, i) => (
+                                <span key={i}>{m} </span>
+                            ))}
+                        </p>
+
+                        <p className="text-gray-600">{item.totalPrice} บาท</p>
+                    </div>
+                    <button
+                        className="opacity-0 translate-x-3 group-hover:opacity-100 group-hover:translate-x-0 transition duration-200 px-4 py-2 rounded-lg bg-red-300 text-white hover:bg-red-600"
+                        onClick={() => console.log("edit")}
+                    >
+                        Edit
+                    </button>
                 </div>
             ))}
         </div>
