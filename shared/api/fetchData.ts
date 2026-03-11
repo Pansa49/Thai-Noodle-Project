74b4204027526329739cfa97796298660f71e2d0
Ping Pansa <Pansa.SaeUe@gmail.com>
2026-03-11 23:30:08 +0700

    shared : fixed url

--- shared/api/fetchData.ts
+++ shared/api/fetchData.ts
@@ -2,7 +2,7 @@
 const URL_STAFF = "https://thai-noodle-lab-staff-db-production.up.railway.app"
 
 export async function getOrderSessionStatus(tableID: string, sessionID: string): Promise<boolean> {
-    const res = await fetch(`${URL_STAFF}/orderSessionsstatus?tableId=${tableID}&orderId=${sessionID}`);
+    const res = await fetch(`${URL_STAFF}/orderSessions?tableId=${tableID}&orderId=${sessionID}`);
     const data = await res.json();
 
     return data[0]?.active ?? false;
