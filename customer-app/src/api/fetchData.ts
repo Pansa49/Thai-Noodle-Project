bd98d95da1fe9536b4d62383759f5ab98df7f605
Ping Pansa <Pansa.SaeUe@gmail.com>
2026-04-02 20:43:48 +0700

    update fectdata : use local

--- customer-app/src/api/fetchData.ts
+++ customer-app/src/api/fetchData.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import type { CartItem } from "../../../shared/menuDetail";
 
-const BASE_URL = "https://thai-noodle-lab-customer-db-production.up.railway.app"
-//const TEST_URL = "http://localhost:3001"
+//const BASE_URL = "https://thai-noodle-lab-customer-db-production.up.railway.app"
+const TEST_URL = "http://localhost:3001"
 
 export async function AddItemDb(orders: CartItem[], tableNo: string, sessionId: string) {
     for (const order of orders) {
@@ -13,7 +13,7 @@ export async function AddItemDb(orders: CartItem[], tableNo: string, sessionId: 
                 sessionId: sessionId,
             };
             await axios.post(
-                `${BASE_URL}/orders`,
+                `${TEST_URL}/orders`,
                 orderData,
 
             );
@@ -25,7 +25,7 @@ export async function AddItemDb(orders: CartItem[], tableNo: string, sessionId: 
 
 export async function getOrdersDb(tableNo: string, sessionId: string) {
     try {
-        const response = await axios.get(`${BASE_URL}/orders?tableNo=${tableNo}&sessionId=${sessionId}&status=ordering`);
+        const response = await axios.get(`${TEST_URL}/orders?tableNo=${tableNo}&sessionId=${sessionId}&status=ordering`);
         console.log("Fetched orders:", response.data);
         return response.data;
     }
