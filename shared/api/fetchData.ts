4b4fc6bd38965268ea6c23910aaf5a931f507c76
Ping Pansa <Pansa.SaeUe@gmail.com>
2026-04-06 16:17:20 +0700

    update fetch data

--- shared/api/fetchData.ts
+++ shared/api/fetchData.ts
@@ -1,8 +1,9 @@
 
-const URL_STAFF = "https://thai-noodle-lab-staff-db-production.up.railway.app"
+// const URL_STAFF = "https://thai-noodle-lab-staff-db-production.up.railway.app"
+const URL_TEST = " http://localhost:4000"
 
 export async function getOrderSessionStatus(tableID: string, sessionID: string): Promise<boolean> {
-    const res = await fetch(`${URL_STAFF}/orderSessions?tableId=${tableID}&orderId=${sessionID}`);
+    const res = await fetch(`${URL_TEST}/orderSessions?tableId=${tableID}&orderId=${sessionID}`);
     const data = await res.json();
 
     return data[0]?.active ?? false;
